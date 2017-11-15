b0b04f91049bfd26875f38ae5070a913046dc554
Julian <yg1811@hotmail.com>
2017-11-14 21:31:06 -0800

    added small sampling rate

--- parse_network.py
+++ parse_network.py
@@ -6,52 +6,52 @@ NETWORK = './data/weibo_network.txt'
 GRAPH = './data/network_graph.txt'
 
 
-def write_to_graph(load_file_name, save_file_name): 
+def write_to_graph(load_file_name, save_file_name, sampling_rate=1): 
 	graph = s.PNGraph.New()
 
 	# 1787443
 	row_number = 0
 	with open(load_file_name) as f:
 		for line in f:
-			row_number += 1
-			if row_number == 1:
-				elements = line.split()
-				n = int(elements[0])
-				m = int(elements[1])
-				graph.Reserve(int(n), int(m))
-				continue
-		
-			#row = line
-			if row_number % 5000 == 0:
-				print(row_number)
 
-			data = line.split('\t')
+			if np.random.random() < sampling_rate:
 
-			node = int(data[0])
-			number = np.array(data[2:], dtype=np.int).reshape((int(data[1]), 2))
+				row_number += 1
+				if row_number == 1:
+					elements = line.split()
+					n = int(elements[0])
+					m = int(elements[1])
+					graph.Reserve(int(n), int(m))
+					continue
+			
+				#row = line
+				if row_number % 5000 == 0:
+					print(row_number)
 
-			for n in number:
+				data = line.split('\t')
 
-				if not graph.IsNode(node):
-					graph.AddNode(node)
-				if not graph.IsNode(n[0]):
-					graph.AddNode(n[0])	
+				node = int(data[0])
+				number = np.array(data[2:], dtype=np.int).reshape((int(data[1]), 2))
 
-				graph.AddEdge(node, n[0])
+				for n in number:
 
-				# If flag marked, reciprocal following relation
-				if n[1]:
-					graph.AddEdge(n[0], node)
+					if not graph.IsNode(node):
+						graph.AddNode(node)
+					if not graph.IsNode(n[0]):
+						graph.AddNode(n[0])	
+
+					graph.AddEdge(node, n[0])
+
+					# If flag marked, reciprocal following relation
+					if n[1]:
+						graph.AddEdge(n[0], node)
 
 	s.SaveEdgeList(graph, save_file_name)
 	return graph
 
-def get_influence_set(graph):
-
-	pass
 
 if __name__ == '__main__':
-	sina = write_to_graph(NETWORK, GRAPH)
+	sina = write_to_graph(NETWORK, GRAPH, 0.08)
 
 	infl = get_influence_set(sina)
 	
