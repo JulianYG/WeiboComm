cc38a384b8e291783068527fa0da2fc76bdd100b
Julian <yg1811@hotmail.com>
2017-11-22 16:10:48 -0800

    skeleton

--- cem.py
+++ cem.py
@@ -9,51 +9,67 @@
 import snap as s 
 import numpy as np 
 
+np.random.seed(42)
+
+class Config:
+    
+    def __init__(self):
+        
+        self.network_file = './data/network_graph_small.txt'
+        self.retweet_file = './data/network_retweet.txt'
+    
+        self.num_examples = 15
+        self.num_top = 4
+        self.epsilon = 1e-3
+        
+
+class NodesStats(object):
+    def __init__(self, node_ids, mu, sig):
+        self.node_ids = node_ids
+        self.mu = mu
+        self.sig = sig
+        self.nodes_mu = {}
+        self.nodes_sig = {}
+    
+    def sample(self):
+        for id in node_ids:
+            self.node_mu `
+
 
 class Graphize(object):
 
 
-	def __init__(self, network_g, retweet_g):
+    def __init__(self, network_g, ):
+        self._network = network_g
 
-		self._network = network_g
-		self._retweet = retweet_g
+    def _find_path(node_stat, ):
+    	pass
 
-
-	def seed(n):
-		np.random.seed(n)
-
-	def assign_prob(sigma, mu):
-
-
-	def 
-
+    def evaluate(node_stat, retweet_g):
+    	
+    	pass
+        
 
 if __name__ == '__main__':
+    
+    # load config
+    conf = Config()
+    prev_scores, scores = np.ones(conf.num_examples), np.zeros(conf.num_examples)
+    
+    sina_network = s.LoadEdgeList(s.PNGraph, conf.sina_network)
+    retweet_graph = s.LoadEdgeList(s.PNGraph, conf.retweet_file)
 
-	# load graphs 
+    graph = Graphize(sina_network)
+    node_stats = NodesStats(sina_network, conf.num_examples)
+    
+    while np.sum((scores - prev_scores) ** 2) > conf.epsilon:
 
-	
+        scores = np.array([(graph.evaluate(node_stats, 
+        	retweet_graph), i) for i in range(conf.num_examples)], dtype=np.float32)
+        top_m = [i for (s, i) in sorted(scores)[:conf.num_top]]
+        
+        # pdate_mu/sigma by refitting mu, sigma on top_m
+        node_stat.update_param(top_m)
 
-	mu, sigma = 0, 0.1
-	eps = 1e10
-
-	while eps > 1e-3:
-
-
-		
-
-		for _ in :
-
-			x = Graphize( a, b)
-			x.assign_prob(mu, sigma)
-
-			...
-
-			score = x.evaluate()
-
-			generate_mu, sigma = np.random.sigma()
-
-			x.assign_prob
-
-
+      
 
--- parse_network.py
+++ parse_network.py
@@ -13,10 +13,9 @@ def write_to_graph(load_file_name, save_file_name, sampling_rate=1):
 	row_number = 0
 	with open(load_file_name) as f:
 		for line in f:
-
 			if np.random.random() < sampling_rate:
-
 				row_number += 1
+
 				if row_number == 1:
 					elements = line.split()
 					n = int(elements[0])
@@ -29,7 +28,6 @@ def write_to_graph(load_file_name, save_file_name, sampling_rate=1):
 					print(row_number)
 
 				data = line.split('\t')
-
 				node = int(data[0])
 				number = np.array(data[2:], dtype=np.int).reshape((int(data[1]), 2))
 
@@ -41,7 +39,7 @@ def write_to_graph(load_file_name, save_file_name, sampling_rate=1):
 						graph.AddNode(n[0])	
 
 					graph.AddEdge(node, n[0])
-
+					
 					# If flag marked, reciprocal following relation
 					if n[1]:
 						graph.AddEdge(n[0], node)
@@ -52,7 +50,6 @@ def write_to_graph(load_file_name, save_file_name, sampling_rate=1):
 
 if __name__ == '__main__':
 	sina = write_to_graph(NETWORK, GRAPH, 0.08)
-
 	infl = get_influence_set(sina)
 	
 
