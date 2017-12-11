22d2543d94f9ddca222a2fa0c576f9830480b0f4
Qiwen Fu <qiwenfu@Qiwens-MacBook-Pro.local>
2017-12-10 22:55:37 -0800

    draw distribution

--- evaluate.py
+++ evaluate.py
@@ -7,7 +7,23 @@ import random
 import pickle
 import numpy as np
 import scipy.stats as sp
+import matplotlib.pyplot as plt
+import math
 
+def assign_prob(graph):
+    probs = {}
+    for node_v in graph.Nodes():
+        in_d = node_v.GetInDeg()
+        r = np.random.uniform(size = in_d)
+        p = r/r.sum()
+        # r = [random.uniform(0,1) for i in range(out_d)]
+        # sum_ = sum(r)
+        # p = [i/sum_ for i in r]
+        v = node_v.GetId()
+        for idx in range(node_v.GetInDeg()):
+            u = node_v.GetInNId(idx)
+            probs[(u,v)] = p[idx]
+    return probs
 
 if __name__ == '__main__':
 
@@ -65,17 +81,101 @@ if __name__ == '__main__':
     with open(conf.ground_truth, 'rb') as f:
         probs = pickle.load(f)
 
-    val_true = np.array([probs[k] for k in node_edge_prob])
-    val_pred_node = np.array([node_edge_prob[k] for k in node_edge_prob])
-    val_pred_edge = np.array([edge_edge_prob[k] for k in node_edge_prob])
+    probs_rand = assign_prob(sina_network)
+    # val_rand = np.array([probs_rand[k] for k in node_edge_prob])
+    # val_true = np.array([probs[k] for k in node_edge_prob])
+    # val_pred_node = np.array([node_edge_prob[k] for k in node_edge_prob])
+    # val_pred_edge = np.array([edge_edge_prob[k] for k in node_edge_prob])
+    keys_not_1 = []
+    for k in node_edge_prob:
+        if probs[k] < 1:
+            keys_not_1.append(k)
+    val_rand = np.array([probs_rand[k] for k in keys_not_1])
+    val_true = np.array([probs[k] for k in keys_not_1])
+    val_pred_node = np.array([node_edge_prob[k] for k in keys_not_1])
+    val_pred_edge = np.array([edge_edge_prob[k] for k in keys_not_1])
+    
+    
+    # print("l2 rand distance: ", 
+    #     np.linalg.norm((np.random.uniform(size=len(node_edge_prob)) - val_true)**2))
+    print("l1 rand distance: ", np.linalg.norm(val_rand - val_true, ord=1))
+    print("l1 distance (node vs true): ", np.linalg.norm(val_pred_node - val_true, ord=1))
+    print("l1 distance (edge vs true): ", np.linalg.norm(val_pred_edge - val_true, ord=1))
 
-    print("l2 rand distance (node vs true): ", 
-        np.linalg.norm((np.random.uniform(size=len(node_edge_prob)) - val_true)**2))
+    print("l2 rand distance: ", np.linalg.norm(val_rand - val_true))
     print("l2 distance (node vs true): ", np.linalg.norm(val_pred_node - val_true))
     print("l2 distance (edge vs true): ", np.linalg.norm(val_pred_edge - val_true))
 
     # TODO: plot prob difference distribution by node degrees
-    degree_avgProb = {}
+    prob_pred = edge_edge_prob
+    degree_avgProb_in = {}
+    degree_avgProb_out = {}
+    for u_node in sina_network.Nodes():
+        u = u_node.GetId()
+        # ## averaging out-links
+        # d_out = u_node.GetOutDeg()
+        # if d_out > 0:
+        #     if(d_out not in degree_avgProb_out):
+        #         degree_avgProb_out[d_out] = []
+        #     avg_out = 0
+        #     for idx in range(d_out):
+        #         v = u_node.GetOutNId(idx)
+        #         avg_out += (abs(prob_pred[(u,v)] - probs[(u,v)])/probs[(u,v)])
+        #     degree_avgProb_out[d_out].append(avg_out/d_out)
+
+        ## averaging in-links
+        d_in = u_node.GetInDeg()
+        if d_in > 0:
+            if(d_in not in degree_avgProb_in):
+                degree_avgProb_in[d_in] = []
+            avg_in = 0
+            for idx in range(d_in):
+                v = u_node.GetInNId(idx)
+                # avg_in += (abs(prob_pred[(v,u)] - probs[(v,u)])/probs[(v,u)])
+                avg_in += (abs(prob_pred[(v,u)] - probs[(v,u)]))
+                # print (prob_pred[(v,u)], probs[(v,u)])
+            degree_avgProb_in[d_in].append(avg_in)
+            # if (avg_in/d_in) > 1:
+            #     print (avg_in/d_in)
+
+    # keys_out = degree_avgProb_out.keys()
+    # keys_out.sort()
+    # avg_out = [sum(degree_avgProb_out[k])/len(degree_avgProb_out[k]) for k in keys_out]
+    keys_in = degree_avgProb_in.keys()
+    keys_in.sort()
+    avg_in = [sum(degree_avgProb_in[k])/len(degree_avgProb_in[k]) for k in keys_in]
+
+    # for k in keys_in:
+    #     if sum(degree_avgProb_in[k])/len(degree_avgProb_in[k]) > 5:
+    #         print (sum(degree_avgProb_in[k])/len(degree_avgProb_in[k]))
+    #         print (degree_avgProb_in[k])
+
+
+
+
+# plt.loglog(keys_in, avg_in,'.',color = 'r', label="In-Link")
+# plt.xlabel('log(Degree)')
+# plt.ylabel('log(Average Percentage Error)')
+keys_in_clipped = []
+avg_in_clipped = []
+for k in range(len(keys_in)):
+    if avg_in[k] < 10 and avg_in[k] > 0:
+        # print (avg_in[k])
+        keys_in_clipped.append(keys_in[k])
+        avg_in_clipped.append(avg_in[k])
+
+avg_in_clipped_log = [math.log(x,0.1) for x in avg_in_clipped] 
+plt.plot(keys_in_clipped, avg_in_clipped_log,'.',color = 'r', label="In-Link")
+# plt.semilogy(keys_in_clipped, avg_in_clipped,'.',color = 'r', label="In-Link")
+plt.xlabel('Degree')
+plt.ylabel('Average Percentage Error')
+plt.title('Error Distribution(Edge Model)')
+
+# plt.plot(keys_out, avg_out,'.', color = 'b', label="Out-Link")
+# plt.legend(loc='upper right', frameon=False)
+plt.show()
+   
+
     
 
 
